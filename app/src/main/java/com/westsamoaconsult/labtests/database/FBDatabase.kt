7d2547309d6759031355a14c07540bd90b17c7c7
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-15 17:45:28 +0800

    Clean code

--- app/src/main/java/com/westsamoaconsult/labtests/database/FBDatabase.kt
+++ app/src/main/java/com/westsamoaconsult/labtests/database/FBDatabase.kt
@@ -30,39 +30,6 @@ class FBDatabase {
         })
     }
 
-    fun flattenDictionary(dict: Map<String, Any>) : Any {
-        val array = mutableListOf<Any>()
-
-        for (i in 0 until dict.keys.size) {
-            array[i] = "***" // just to make sure the array is not empty
-        }
-
-        if (dict.keys.first().matches(Regex(".*\\d+.*"))) {
-            for (key in dict.keys) {
-                if (dict[key] is Map<*, *>) {
-                    array[key.toInt()] = flattenDictionary(dict[key] as Map<String, Any>)
-                } else {
-                    array[key.toInt()] = dict[key] as Map<String, Any>
-                }
-            }
-
-            array.remove("***")
-            return array
-        } else {
-            val dictionary = mutableMapOf<String, Any>()
-
-            for (key in dict.keys) {
-                if (dict[key] is Map<*, *>) {
-                    dictionary[key] = flattenDictionary(dict[key] as Map<String, Any>)
-                } else {
-                    dictionary[key] = dict[key] as Map<String, Any>
-                }
-            }
-
-            return dictionary
-        }
-    }
-
     fun articleWithNumericAddress(numericAddress: Int) : Map<String, Any>?{
         val array = articles["Articles"] as List<Any>
 
