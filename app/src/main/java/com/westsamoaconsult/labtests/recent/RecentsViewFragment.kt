19452b8aaf2575ab8fc0eae354ae4824cccc4274
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-15 23:50:41 +0800

    RecentsView Fragment

---
+++ app/src/main/java/com/westsamoaconsult/labtests/recent/RecentsViewFragment.kt
@@ -0,0 +1,50 @@
+package com.westsamoaconsult.labtests.recent
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.westsamoaconsult.labtests.MainApplication
+import com.westsamoaconsult.labtests.R
+import com.westsamoaconsult.labtests.bookmark.detail.DetailViewFragment
+import com.westsamoaconsult.labtests.bookmark.second.SecondViewAdapter
+import com.westsamoaconsult.labtests.common.BaseActivity
+import com.westsamoaconsult.labtests.common.BaseFragment
+import com.westsamoaconsult.labtests.database.ArticleItem
+import com.westsamoaconsult.labtests.utils.Utils
+import kotlinx.android.synthetic.main.info_fragment.*
+
+
+class RecentsViewFragment: BaseFragment(), SecondViewAdapter.OnItemClickListener {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(com.westsamoaconsult.labtests.R.layout.bookmark_first_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        onForeground()
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+    }
+
+    override fun onClick(article: ArticleItem) {
+        Utils.addFragment(DetailViewFragment.newInstance(article), activity!!.supportFragmentManager, R.id.fragmentContainer)
+    }
+
+    override fun onForeground() {
+        super.onForeground()
+
+        (activity as BaseActivity).apply {
+            setTitle("Recents")
+            setRightButtonVisible(true, "Clear")
+        }
+
+        recyclerView.adapter = RecentsViewAdapter(MainApplication.instance.database.getRecents(), this)
+    }
+
+    override fun onRightButtonPressed() {
+        MainApplication.instance.database.clearRecents()
+        recyclerView.adapter = RecentsViewAdapter(MainApplication.instance.database.getRecents(), this)
+    }
+}
