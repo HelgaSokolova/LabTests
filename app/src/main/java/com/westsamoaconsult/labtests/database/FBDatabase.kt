823fb59b45229982f0f2bcfd81080e5910d23039
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-15 17:44:31 +0800

    Fix FB indexing

--- app/src/main/java/com/westsamoaconsult/labtests/database/FBDatabase.kt
+++ app/src/main/java/com/westsamoaconsult/labtests/database/FBDatabase.kt
@@ -14,62 +14,62 @@ class FBDatabase {
     private fun retrieveArticles() {
         ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.getValue()?.let {
+                dataSnapshot.value?.let {
                     articles = mutableMapOf<String, Any>()
                     articles["Articles"] = it
-                    usable = true;
-                    return;
+                    usable = true
+                    return
                 }
                 usable = false
             }
 
             override fun onCancelled(error: DatabaseError) {
                 // Failed to read value
-                usable = false;
+                usable = false
             }
         })
     }
 
     fun flattenDictionary(dict: Map<String, Any>) : Any {
-        val array = mutableListOf<Any>();
+        val array = mutableListOf<Any>()
 
         for (i in 0 until dict.keys.size) {
-            array[i] = "***"; // just to make sure the array is not empty
+            array[i] = "***" // just to make sure the array is not empty
         }
 
         if (dict.keys.first().matches(Regex(".*\\d+.*"))) {
             for (key in dict.keys) {
                 if (dict[key] is Map<*, *>) {
-                    array[key.toInt()] = flattenDictionary(dict[key] as Map<String, Any>);
+                    array[key.toInt()] = flattenDictionary(dict[key] as Map<String, Any>)
                 } else {
-                    array[key.toInt()] = dict[key] as Map<String, Any>;
+                    array[key.toInt()] = dict[key] as Map<String, Any>
                 }
             }
 
             array.remove("***")
-            return array;
+            return array
         } else {
-            val dictionary = mutableMapOf<String, Any>();
+            val dictionary = mutableMapOf<String, Any>()
 
             for (key in dict.keys) {
                 if (dict[key] is Map<*, *>) {
-                    dictionary[key] = flattenDictionary(dict[key] as Map<String, Any>);
+                    dictionary[key] = flattenDictionary(dict[key] as Map<String, Any>)
                 } else {
-                    dictionary[key] = dict[key] as Map<String, Any>;
+                    dictionary[key] = dict[key] as Map<String, Any>
                 }
             }
 
-            return dictionary;
+            return dictionary
         }
     }
 
     fun articleWithNumericAddress(numericAddress: Int) : Map<String, Any>?{
         val array = articles["Articles"] as List<Any>
 
-        if (numericAddress - 1 < array.size) {  // -1 due to 0 indexing
-            return array[numericAddress - 1] as Map<String, Any>;
+        if (numericAddress < array.size) {  // -1 due to 0 indexing
+            return array[numericAddress] as Map<String, Any>
         }
 
-        return null;
+        return null
     }
 }
