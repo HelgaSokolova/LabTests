38490668a929792f38a3b8da669e170a9155f708
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-18 16:53:52 +0800

    Fix line spacing issue

--- app/src/main/java/com/westsamoaconsult/labtests/tabs/bookmark/detail/HeaderDetailViewHolder.kt
+++ app/src/main/java/com/westsamoaconsult/labtests/tabs/bookmark/detail/HeaderDetailViewHolder.kt
@@ -3,6 +3,12 @@ package com.westsamoaconsult.labtests.tabs.bookmark.detail
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.synthetic.main.detail_item_header.view.*
+import android.R.attr.scrollY
+import android.R.attr.scrollY
+
+
+
+
 
 
 class HeaderDetailViewHolder(itemView: View): RecyclerView.ViewHolder(itemView),
@@ -17,13 +23,20 @@ class HeaderDetailViewHolder(itemView: View): RecyclerView.ViewHolder(itemView),
             firstText.text = item.description
             firstText.viewTreeObserver.addOnGlobalLayoutListener {
                 firstText.apply {
-                    val lastVisibleLineNumber = layout.getLineForVertical(scrollY + height)
+                    val lastVisibleLineNumber = height / lineHeight - 1
                     val end = layout.getLineEnd(lastVisibleLineNumber)
+                    text = item.description.substring(0, end)
                     itemView.secondText.text = item.description.substring(end, item.description.length)
                     if (itemView.secondText.text.isEmpty()) {
                         itemView.secondText.visibility = View.GONE
                     }
                 }
+            }
+
+            firstText.viewTreeObserver.addOnPreDrawListener {
+                val lineCounts = firstText.layout.lineCount
+                var k = 5
+                true
             }
 
             val imageId = context.resources.getIdentifier(logo, "drawable", context.packageName)
