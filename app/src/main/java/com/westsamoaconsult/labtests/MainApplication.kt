d1b7139385f73dd378dd73ce4d5a2cd0071f70c7
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-19 14:14:19 +0800

    BillingProcessor initialization

--- app/src/main/java/com/westsamoaconsult/labtests/MainApplication.kt
+++ app/src/main/java/com/westsamoaconsult/labtests/MainApplication.kt
@@ -1,25 +1,45 @@
 package com.westsamoaconsult.labtests
 
 import android.app.Application
+import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.TransactionDetails
 import com.westsamoaconsult.labtests.database.Database
 import com.westsamoaconsult.labtests.database.FBDatabase
 import com.westsamoaconsult.labtests.utils.Constants
-import com.westsamoaconsult.labtests.utils.Utils
 
-class MainApplication : Application() {
+
+class MainApplication : Application(), BillingProcessor.IBillingHandler {
     companion object {
         lateinit var instance: MainApplication
             private set
     }
     lateinit var database: Database
-
     lateinit var FBDatabase: FBDatabase
+    lateinit var bp: BillingProcessor
+
     override fun onCreate() {
         super.onCreate()
-
         instance = this
 
         database = Database()
         FBDatabase = FBDatabase()
+        bp = BillingProcessor(this, Constants.APP_LICENSE_KEY, this)
+        bp.initialize()
+    }
+
+    override fun onBillingInitialized() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPurchaseHistoryRestored() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProductPurchased(productId: String, details: TransactionDetails?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBillingError(errorCode: Int, error: Throwable?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/com/westsamoaconsult/labtests/utils/Constants.kt
+++ app/src/main/java/com/westsamoaconsult/labtests/utils/Constants.kt
@@ -7,4 +7,5 @@ object Constants {
     const val Last_Opened_Times = "LastOpenedTimes"
     const val Favorites = "Favorites"
     const val NotFirstRun = "NotFirstRun"
+    const val APP_LICENSE_KEY = "IIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArHchZLd8SQSbebJuQwuQwfzLXqfLwwKoHMI+Ditj598FDQcQTXmqJfBCss2Qs0Vz2MZDXGSF57J9m3Ei4rnhoyEGJuSq43VkgcLLX303Rxpfn8cnwLUHgGKfNYoLFGH3JKA1iFM77d49blOYOly1TIGCTAWkgpGRyZ6KUE8nOVr89UN7LlPED/HmMgCLs4lItq9eyCuFF1of0JYT59w5Pdd/eOKZj/1o/LWZfoHf7BN/6hiQjaYi6qZbDkacDWCNdq4lrrrwJQpn8fYOEBjEYpWAsoB6FlHV1wNtbzvyGSaI8NcGZE6OSdTfLCqlm47kaLbzLZFaks3y/6UKnPQIzwIDAQAB"
 }
